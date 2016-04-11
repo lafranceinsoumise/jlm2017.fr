6c3664e7a5465eaa714007f1179989a1d8313434
Guillaume Royer <perso@guilro.com>
2016-04-11 13:23:32 +0200

    importing NB theme BS variables and navbar

--- web/app/themes/jlm2017/templates/header.php
+++ web/app/themes/jlm2017/templates/header.php
@@ -1,12 +1,14 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+  <nav class="nav-primary navbar navbar-default navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      </div>
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav navbar-right']);
       endif;
       ?>
-    </nav>
-  </div>
+    </div>
+  </nav>
 </header>
