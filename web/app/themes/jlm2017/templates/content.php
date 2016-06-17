fec9c27fa8d47b8045c25257cc1d5823cc024de3
Guillaume Royer <perso@guilro.com>
2016-06-17 17:26:24 +0200

    video in archive view for video post format

--- web/app/themes/jlm2017/templates/content.php
+++ web/app/themes/jlm2017/templates/content.php
@@ -1,8 +1,18 @@
 <article <?php post_class(); ?>>
   <div class="content">
-    <?php if (has_post_thumbnail()) {
+    <?php if (get_post_type() === 'post' && get_post_format() === 'video') {
+      $content = do_shortcode(apply_filters('the_content', get_the_content()));
+      $embeds = get_media_embedded_in_content($content);
+      if(!empty($embeds)) { ?>
+        <div class="entry-content-asset">
+          <?php echo $embeds[0]; ?>
+        </div>
+      <?php }
+      //var_dump(get_the_content());
+      //var_dump(get_media_embedded_in_content());
+    } else if (has_post_thumbnail()) {
       the_post_thumbnail('large');
-    } ?>
+    }?>
     <header>
       <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <?php get_template_part('templates/entry-meta'); ?>
