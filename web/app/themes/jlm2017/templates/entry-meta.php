8b6cda80d87bda9af86cc965a1a5fcac270acd51
Guillaume Royer <perso@guilro.com>
2016-06-28 11:24:33 +0200

    styling meta and signe header

--- web/app/themes/jlm2017/templates/entry-meta.php
+++ web/app/themes/jlm2017/templates/entry-meta.php
@@ -1,2 +1,4 @@
+<span class="text-muted">
 <time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
 dans <a href="<?= get_category_link(get_the_category()[0]->cat_ID); ?>"><?= get_the_category()[0]->name; ?></a>
+</span>
