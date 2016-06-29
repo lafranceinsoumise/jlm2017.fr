8300f5bdbcbe425c3b8586922a5e65b8092a5e2e
Guillaume Royer <perso@guilro.com>
2016-06-29 23:14:16 +0200

    navbar fixed and collapsed

--- web/app/themes/jlm2017/templates/header.php
+++ web/app/themes/jlm2017/templates/header.php
@@ -1,7 +1,10 @@
 <header class="banner">
-  <nav class="nav-primary navbar navbar-default navbar-static-top">
+  <nav class="nav-primary navbar navbar-default navbar-fixed-top">
     <div class="container">
       <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar-collapse" aria-expanded="false">
+          Menu <span class="caret"></span>
+        </button>
         <?php
           if (has_custom_logo()) {
             the_custom_logo();
@@ -12,15 +15,17 @@
           }
         ?>
       </div>
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu([
-          'theme_location' => 'primary_navigation',
-          'walker' => new wp_bootstrap_navwalker(),
-          'menu_class' => 'nav navbar-nav navbar-right'
-        ]);
-      endif;
-      ?>
+      <div class="collapse navbar-collapse" id="main-navbar-collapse">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu([
+            'theme_location' => 'primary_navigation',
+            'walker' => new wp_bootstrap_navwalker(),
+            'menu_class' => 'nav navbar-nav navbar-right'
+          ]);
+        endif;
+        ?>
+      </div>
     </div>
   </nav>
 </header>
