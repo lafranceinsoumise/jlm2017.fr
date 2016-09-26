48bfcf9aaf88f0e05a6a49b053860815b142c89e
Guillaume Royer <perso@guilro.com>
2016-09-26 13:31:25 +0200

    display all categories

--- web/app/themes/jlm2017/templates/entry-meta.php
+++ web/app/themes/jlm2017/templates/entry-meta.php
@@ -1,4 +1,13 @@
 <span class="text-muted">
 <time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
-dans <a href="<?= get_category_link(get_the_category()[0]->cat_ID); ?>"><?= get_the_category()[0]->name; ?></a>
+dans
+<?php
+  $categories = get_the_category();
+
+  foreach ($categories as $key => $category) {
+?>
+<a href="<?= $category->cat_ID ?>">
+  <?= $category->name; ?>
+</a><?= $key < count($categories) - 1 ? ', ': '' ?>
+<?php } ?>
 </span>
